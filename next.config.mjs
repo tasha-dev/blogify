e459eca5dd9c0e896fb63e12c9cdd690145ff8fc
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-03-28 14:26:28 +0330

    added comments in configs files (nextjs and tailwind)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// Codes by mahdi tasha
+// Importing type of configs of nextJS
 /** @type {import('next').NextConfig} */
+
+// Defining the configs
 const nextConfig = {};
 
+// Exporting the configs
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,13 @@
+// Codes by mahdi tasha
+// Importing type of configs of tailwindCSS
 import type { Config } from "tailwindcss";
 
+// Defining the configs
 const config: Config = {
   content: [],
   theme: {},
   plugins: [],
 };
+
+// Exporting the configs
 export default config;
